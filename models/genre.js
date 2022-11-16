beda41cf3570a05cdc9b25c48129bc8eab84b87a
Harsh Sachala <sach46229@gmail.com>
2022-11-16 13:00:16 +0530

    new genre model

---
+++ models/genre.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+const Genre = new mongoose.model("Genre", genreSchema);
+
+function validateGenre(genre) {
+  const genreSchema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+  });
+
+  return genreSchema.validate(genre);
+}
+
+module.exports.Genre = Genre;
+module.exports.validateGenre = validateGenre;
